a6da8d37bfb3d2535844acffb035455f3ad37a68
SleepyMode <sleepy@sleepymode.me>
2023-08-11 11:17:18 +0300

    handlers: Use indexer access on 'any' objects

--- src/handlers/default.ts
+++ src/handlers/default.ts
@@ -13,9 +13,9 @@ export abstract class DefaultHandlers {
     static async commitPlayer(json: any) {
         await Database.getClient().player.create({
             data: {
-                id: json.playerId as string,
-                steamName: json.steamName as string,
-                steamId: json.steamId as string
+                id: json["playerId"] as string,
+                steamName: json["steamName"] as string,
+                steamId: json["steamId"] as string
             }
         });
 
@@ -27,14 +27,14 @@ export abstract class DefaultHandlers {
     static async savePlayer(json: any) {
         await Database.getClient().player.update({
             where: {
-                id: json.playerId as string
+                id: json["playerId"] as string
             },
             data: {
-                steamName: json.steamName as string,
-                whitelists: json.whitelist as string[],
-                blacklists: json.blacklist as string[],
+                steamName: json["steamName"] as string,
+                whitelists: json["whitelist"] as string[],
+                blacklists: json["blacklist"] as string[],
                 lastSeen: new Date(),
-                data: json.data as string
+                data: json["data"] as string
             }
         });
 
@@ -46,7 +46,7 @@ export abstract class DefaultHandlers {
     static async loadCharacters(json: any) {
         const characters = await Database.getClient().character.findMany({
             where: {
-                playerId: json.playerId as string
+                playerId: json["playerId"] as string
             }
         });
 
@@ -62,11 +62,11 @@ export abstract class DefaultHandlers {
     static async commitCharacter(json: any) {
         await Database.getClient().character.create({
             data: {
-                id: json.characterId as string,
-                playerId: json.playerId as string,
-                name: json.name as string,
-                model: json.model as string,
-                faction: json.faction as string
+                id: json["characterId"] as string,
+                playerId: json["playerId"] as string,
+                name: json["name"] as string,
+                model: json["model"] as string,
+                faction: json["faction"] as string
             }
         });
 
@@ -78,13 +78,13 @@ export abstract class DefaultHandlers {
     static async saveCharacter(json: any) {
         await Database.getClient().character.update({
             where: {
-                id: json.characterId as string
+                id: json["characterId"] as string
             },
             data: {
-                name: json.name as string,
-                model: json.model as string,
-                faction: json.faction as string,
-                data: json.data as string
+                name: json["name"] as string,
+                model: json["model"] as string,
+                faction: json["faction"] as string,
+                data: json["data"] as string
             }
         });
 
